f379ced1cda5152ec07b8fdd40f85a493b617c78
Max <v0id@list.ru>
2019-08-25 14:56:43 +0200

    Add Makefile + test fix

--- options_test.go
+++ options_test.go
@@ -11,21 +11,22 @@ func TestOptions_Validate(t *testing.T) {
 
 	options.SourceFile = "path/to/source_file"
 	if err := options.Validate(); err == ErrorSourcePathEmpty {
-		t.Error("Wrong validation error (expect ErrorSourcePathEmpty)")
+		t.Error("Wrong validation error (unexpected ErrorSourcePathEmpty)")
 	}
 
 	options.ResultFile = "path/to/result_file"
 	if err := options.Validate(); err == ErrorResultPathEmpty {
-		t.Error("Wrong validation error (expect ErrorSourcePathEmpty)")
+		t.Error("Wrong validation error (unexpected ErrorSourcePathEmpty)")
 	}
 
-	options.Offset = 0
-	if err := options.Validate(); err == ErrorOffsetLessZero {
+	options.Offset = -1
+	if err := options.Validate(); err != ErrorOffsetLessZero {
 		t.Error("Wrong validation error (expect ErrorOffsetLessZero)")
 	}
 
+	options.Offset = 0
 	options.BlockSize = 1024
 	if err := options.Validate(); err == ErrorBlockSizeLessZero {
-		t.Error("Wrong validation error (expect ErrorBlockSizeLessZero)")
+		t.Error("Wrong validation error (unexpected ErrorBlockSizeLessZero)")
 	}
 }
